8f7b4e2f3851ea94863700e10b09ad5fbb78f8aa
Rafael Amarantes <rafael.amarantes@racco.com>
2019-03-14 16:57:30 -0300

    create a basic structure react router for streams

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,21 @@
 import React from 'react'
 import {BrowserRouter, Route} from 'react-router-dom'
+import StreamList from './Streams/StreamList'
+import StreamShow from './Streams/StreamShow'
+import StreamEdit from './Streams/StreamEdit'
+import StreamDelete from './Streams/StreamDelete'
+import StreamCreate from './Streams/StreamCreate'
+import Header from './Header'
 
 
-export default () => {
-    return <div>App</div>
-}
+export default () => 
+    <div className="ui container">
+        <BrowserRouter>
+        <Header />
+        <Route path="/" exact  component={StreamList} />        
+        <Route path="/streams/new" component={StreamCreate} />
+        <Route path="/streams/edit"  component={StreamEdit} />
+        <Route path="/streams/delete" component={StreamDelete} />
+        <Route path="/streams/show"  component={StreamShow} />
+        </BrowserRouter>
+    </div>
---
+++ client/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+export default () => <div className="ui secondary pointing menu">
+    <Link to="/" className="item">Streamer</Link>
+    <div className="right menu">
+        <Link to="/" className="item">All streams</Link>
+        
+    </div>
+
+</div>
